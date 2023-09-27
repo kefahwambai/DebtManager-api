e422faa4827c355df7a04b6bbd7c22a5bde2600d
KefahWambai <kefah.ngwei@student.moringaschool.com>
2023-09-27 18:34:07 +0300

    deployment env variables

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,6 +1,18 @@
-set -o errexit
+# set -o errexit
 
-bundle install
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
-bundle exec rake db:migrate
+# bundle install
+# bundle exec rake assets:precompile
+# bundle exec rake assets:clean
+# bundle exec rake db:migrate
+
+# #!/bin/bash
+set -e
+
+
+bundle check || bundle install
+bundle exec rails assets:precompile
+bundle exec rails assets:clean
+bundle exec rails db:migrate
+
+# Exit with a success message
+echo "Deployment completed successfully."
