48eda93f5a44d140311fa9d5fe206b785df1a4ec
KefahWambai <kefah.ngwei@student.moringaschool.com>
2023-09-27 19:13:03 +0300

    edited bins folder

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,9 +1,9 @@
 set -o errexit
 
 bundle install
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
-bundle exec rake db:migrate
+bundle exec rails assets:precompile
+bundle exec rails assets:clean
+bundle exec rails db:migrate
 
 # #!/bin/bash
 # set -e
