5bd7d8451a34154c4c2d8f113e0fa32e406b12d3
KefahWambai <kefah.ngwei@student.moringaschool.com>
2023-09-28 11:18:41 +0300

    default_url config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,8 +1,9 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.routes.default_url_options = {
-  host: 'http://localhost:3000'
+  host: 'https://debtmanager.onrender.com'
 }
+
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
